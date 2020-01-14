db936c737bc1328325092c81d5dab7ca121fcbd5
Jordan Pietkewicz <j.pietkewicz@gmail.com>
2020-01-14 09:28:14 -0500

    Split all strings into individual words, deleted whitespace and single character (non-alpha) strings, all lowercase for comparison

--- Projects/Module 2 Assignment/src/WebpageWordCount.java
+++ Projects/Module 2 Assignment/src/WebpageWordCount.java
@@ -1,43 +1,33 @@
-import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.*;
-
 import org.htmlparser.beans.StringBean;
-import org.htmlparser.util.ParserException;
 
 public class WebpageWordCount {
 	
 	/**
-	 * -main gets all text from webpage (including HTML tags)
-	 * 1. research ways to get without tags
-	 * 2. add try/catch
+	 * -main gets all text from web page, separates into individual words while
+	 * clearing out single characters and bringing everything to lower case to compare
 	 * @param args
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {
-		
-//		// get text with BufferedReader (still shows tags)
-//		List<String> wordsWithHTML = new ArrayList<>();
-//		URL macbeth = new URL("http://shakespeare.mit.edu/macbeth/full.html");
-//        BufferedReader in = new BufferedReader(new InputStreamReader(macbeth.openStream()));
-//        String inputLine;
-//        while ((inputLine = in.readLine()) != null) {
-//            wordsWithHTML.add(inputLine);
-//        }
-//        in.close();
+	public static void main(String[] args) {
 		
 		// Gets text with StringBean (no tags)!! 
+		List<String> allWords = new ArrayList<>();
+
 		StringBean sb = new StringBean();
 		sb.setURL("http://shakespeare.mit.edu/macbeth/full.html");
 		String content = sb.getStrings();
-		List<String> allWords = new ArrayList<>();
-		for (String s : content.split(" ")) {
-			allWords.add(s);
+		
+		for (String s : content.split("\\s+")) {
+			s = s.replaceAll("[^a-zA-z]", "").toLowerCase();
+			if (s.isEmpty()) {
+			}
+			else {
+				allWords.add(s);
+			}
 		}
 		
-		System.out.println(allWords);
-		//System.out.println(content);
 	}
 
 }
