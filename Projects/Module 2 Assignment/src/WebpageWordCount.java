33f68e1bf9d6b89c9511c26f9b7e76c3a6c70af6
Jordan Pietkewicz <j.pietkewicz@gmail.com>
2020-01-13 21:25:04 -0500

    Added code to get all text from page (including tags)

--- Projects/Module 2 Assignment/src/WebpageWordCount.java
+++ Projects/Module 2 Assignment/src/WebpageWordCount.java
@@ -1,8 +1,31 @@
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
 
 public class WebpageWordCount {
+	
+	/**
+	 * -main gets all text from webpage (including HTML tags)
+	 * 1. research ways to get without tags
+	 * 2. add try/catch
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		List<String> wordsWithHTML = new ArrayList<>();
+		URL macbeth = new URL("http://shakespeare.mit.edu/macbeth/full.html");
+        BufferedReader in = new BufferedReader(new InputStreamReader(macbeth.openStream()));
 
-	public static void main(String[] args) {
-		
+        String inputLine;
+        
+        while ((inputLine = in.readLine()) != null) {
+            wordsWithHTML.add(inputLine);
+        }
+        
+        
+        
+        in.close();
 	}
 
 }
