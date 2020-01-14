ad1266b0494ddc97db5d38eff55439f1450b004e
Jordan Pietkewicz <j.pietkewicz@gmail.com>
2020-01-13 22:30:55 -0500

    Split into strings without tags, still contains \r and \n

--- Projects/Module 2 Assignment/src/WebpageWordCount.java
+++ Projects/Module 2 Assignment/src/WebpageWordCount.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 
+import org.htmlparser.beans.StringBean;
+import org.htmlparser.util.ParserException;
+
 public class WebpageWordCount {
 	
 	/**
@@ -13,19 +16,28 @@ public class WebpageWordCount {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		List<String> wordsWithHTML = new ArrayList<>();
-		URL macbeth = new URL("http://shakespeare.mit.edu/macbeth/full.html");
-        BufferedReader in = new BufferedReader(new InputStreamReader(macbeth.openStream()));
-
-        String inputLine;
-        
-        while ((inputLine = in.readLine()) != null) {
-            wordsWithHTML.add(inputLine);
-        }
-        
-        
-        
-        in.close();
+		
+//		// get text with BufferedReader (still shows tags)
+//		List<String> wordsWithHTML = new ArrayList<>();
+//		URL macbeth = new URL("http://shakespeare.mit.edu/macbeth/full.html");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(macbeth.openStream()));
+//        String inputLine;
+//        while ((inputLine = in.readLine()) != null) {
+//            wordsWithHTML.add(inputLine);
+//        }
+//        in.close();
+		
+		// Gets text with StringBean (no tags)!! 
+		StringBean sb = new StringBean();
+		sb.setURL("http://shakespeare.mit.edu/macbeth/full.html");
+		String content = sb.getStrings();
+		List<String> allWords = new ArrayList<>();
+		for (String s : content.split(" ")) {
+			allWords.add(s);
+		}
+		
+		System.out.println(allWords);
+		//System.out.println(content);
 	}
 
 }
