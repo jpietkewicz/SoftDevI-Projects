8bc50d675156ea62fbddb6f0727299e3155c4738
Jordan Pietkewicz <j.pietkewicz@gmail.com>
2020-01-27 21:10:39 -0500

    Done!

--- Projects/Module 2 Assignment/src/WebpageWordCount.java
+++ Projects/Module 2 Assignment/src/WebpageWordCount.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.htmlparser.beans.StringBean;
@@ -12,11 +12,10 @@ import org.htmlparser.beans.StringBean;
 // changed to work with my specific code
 //Custom Comparator to sort the map according to the natural 
 //ordering of its values
-class CustomComparator<String,Integer extends Comparable> implements Comparator<String>
-{
-	private Map<String,Integer> map;
+class CustomComparator<String, Integer extends Comparable> implements Comparator<String> {
+	private Map<String, Integer> map;
 
-	public CustomComparator(Map<String,Integer> map) {
+	public CustomComparator(Map<String, Integer> map) {
 		this.map = new HashMap<>(map);
 	}
 
@@ -27,66 +26,62 @@ class CustomComparator<String,Integer extends Comparable> implements Comparator<
 }
 
 public class WebpageWordCount {
-	
+
 	// Generic function to sort Map by values using TreeMap
-		public static  <String, Integer> Map<String, Integer> sortByValues(Map<String, Integer> map)
-		{
-			Comparator<String> comparator = new CustomComparator(map);
+	public static <String, Integer> Map<String, Integer> sortByValues(Map<String, Integer> map) {
+		Comparator<String> comparator = new CustomComparator(map);
 
-			Map<String,Integer> sortedMap = new TreeMap<>(comparator);
-			sortedMap.putAll(map);
+		Map<String, Integer> sortedMap = new TreeMap<>(comparator);
+		sortedMap.putAll(map);
 
-			return sortedMap;
-		}
-	
+		return sortedMap;
+	}
+
 	/**
-	 * -main gets all text from web page, separates into individual words while
-	 * clearing out single characters and bringing everything to lower case to compare
+	 * main gets all text from web page, separates into individual words while
+	 * clearing out single characters and bringing everything to lower case to
+	 * compare
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		List<String> allWords = new ArrayList<>();
 
-		// Gets text with StringBean (no tags)!! 
+		// Gets text with StringBean (no tags)!!
 		StringBean sb = new StringBean();
 		sb.setURL("http://shakespeare.mit.edu/macbeth/full.html");
 		String content = sb.getStrings();
-		
-		// split by all whitespace, take out '-' and downcast capitals, add all words to one list
+
+		// split by all whitespace, take out '-' and downcast capitals, add all words to
+		// one list
 		for (String s : content.split("\\s+")) {
 			s = s.replaceAll("[^a-zA-z]", "").toLowerCase();
 			if (!(s.isEmpty())) {
 				allWords.add(s);
 			}
 		}
-		
+
 		// put all words into map and add counts
 		Map<String, Integer> wordMap = new HashMap<>();
 		for (String s : allWords) {
 			if (!(wordMap.containsKey(s))) {
 				wordMap.put(s, 1);
-			}
-			else {
+			} else {
 				Integer count = wordMap.get(s);
 				count++;
 				wordMap.replace(s, count);
 			}
 		}
-		
+
 		// sort by value
 		wordMap = sortByValues(wordMap);
-		
-		System.out.println("Top 20 words in Macbeth (by amount of appearances)");
-		System.out.println("--------------------------------------------------");
-		
-		int j = 0;
-		for (String s : wordMap.keySet()) {
-				System.out.println(j+1 + ". " + s);
-				j++;
-				if (j >= 20) {
-					break;
-				}
+
+		System.out.println("Words in Macbeth (by number of appearances)");
+		System.out.println("-------------------------------------------");
+
+		for (Entry<String, Integer> e : wordMap.entrySet()) {
+			System.out.println(e.getKey() + ": " + e.getValue());
 		}
 	}
 
